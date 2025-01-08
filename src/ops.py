1e076943a5d1171ec30f9702e69061add10d3af9
Uday Patel <udayraj2005@gmail.com>
2025-01-08 11:13:55 +0530

    all files

---
+++ src/ops.py
@@ -0,0 +1,11 @@
+def ADD(a,b):
+    return a+b
+
+def SUB(a,b):
+    return a-b
+
+    
+
+
+
+    
---
+++ tests/test_ops.py
@@ -0,0 +1,4 @@
+from src.ops import ADD,SUB
+def test_add_sub():
+    assert ADD(5,4)==9
+    assert SUB(12,3)==9
